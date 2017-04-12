d3e9dfc3af1c41a1293f6f70c370f06f701bec57
SeongTae Jeong <kevin13@chol.net>
2017-04-12 23:15:28 +0900

    - splash window - move inkscape-res from resources  to .zip file

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,16 +5,50 @@ using System.IO;
 using System.Collections.ObjectModel;
 using SvgConverter.Properties;
 using System.Diagnostics;
+using System.Resources;
+using ICSharpCode.SharpZipLib.Zip;
+using ICSharpCode.SharpZipLib.Core;
+using System.Windows.Controls;
 
 namespace SvgConverter
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// SVG Splash & Icons: designed by {dimitry-miroliubov} from Flaticon
+    ///                     http://www.flaticon.com/authors/dimitry-miroliubov
+    ///                     http://www.flaticon.com/free-icon/svg_337954#term=SVG&page=1&position=2
     /// </summary>
+    
+    /// <summary>
+    /// PNG to Icon
+    /// http://convertico.com/
+    /// </summary>
+
     public partial class MainWindow : Window
     {
         ObservableCollection<FileItem> _svgList = new ObservableCollection<FileItem>();
         ObservableCollection<FileItem> _convertedList = new ObservableCollection<FileItem>();
+
+        public string InkscapePath
+        {
+            get
+            {
+                string exePath = typeof(MainWindow).Assembly.Location;
+                string binFolderPath = Path.Combine(Path.GetDirectoryName(exePath), "Inkscape");
+
+                return binFolderPath;
+            }
+        }
+
+        public string ZipFilePath
+        {
+            get
+            {
+                string exePath = typeof(MainWindow).Assembly.Location;
+                string zipFilePath = Path.Combine(Path.GetDirectoryName(exePath), "Inkscape.zip");
+
+                return zipFilePath;
+            }
+        }
 
         public MainWindow()
         {
@@ -24,6 +58,8 @@ namespace SvgConverter
             this.lvConverted.ItemsSource = _convertedList;
 
             LoadFromContext();
+
+            ExtractResources();
         }
 
         private void LoadFromContext()
@@ -38,6 +74,37 @@ namespace SvgConverter
         {
             Settings.Default.Path = this.txtFolderPath.Text;
             Settings.Default.Save();
+        }
+
+        private void ExtractResources()
+        {
+            // https://github.com/icsharpcode/SharpZipLib/wiki/Zip-Samples#unpack-a-zip-using-zipinputstream-eg-for-unseekable-input-streams
+            if (Directory.Exists(InkscapePath) == false)
+            {
+                using (FileStream stream = File.OpenRead(ZipFilePath))
+                {
+                    ZipInputStream zipInputStream = new ZipInputStream(stream);
+                    ZipEntry entry = zipInputStream.GetNextEntry();
+                    while (entry != null)
+                    {
+                        string entryFileName = entry.Name;
+
+                        byte[] buffer = new byte[4096];     // 4K is optimum
+
+                        String fullZipToPath = Path.Combine(InkscapePath, entryFileName);
+                        string directoryName = Path.GetDirectoryName(fullZipToPath);
+                        if (directoryName.Length > 0)
+                            Directory.CreateDirectory(directoryName);
+
+                        using (FileStream streamWriter = File.Create(fullZipToPath))
+                        {
+                            StreamUtils.Copy(zipInputStream, streamWriter, buffer);
+                        }
+
+                        entry = zipInputStream.GetNextEntry();
+                    }
+                }
+            }
         }
 
         private void txtFolderPathChanged(object sender, RoutedEventArgs e)
@@ -58,9 +125,13 @@ namespace SvgConverter
 
         private void btnFolderClicked(object sender, RoutedEventArgs e)
         {
-            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
-            dialog.IsFolderPicker = true;
-            dialog.EnsurePathExists = true;
+            var dialog = new CommonOpenFileDialog
+            {
+                EnsurePathExists = true,
+                EnsureFileExists = true,
+                AllowNonFileSystemItems = false,
+                IsFolderPicker = true
+            };
 
             CommonFileDialogResult result = dialog.ShowDialog();
             if (result == CommonFileDialogResult.Ok)
@@ -85,6 +156,11 @@ namespace SvgConverter
             }
         }
 
+        private void lvSVGSelectionChanged(object sender, RoutedEventArgs e)
+        {
+            btnConvert.IsEnabled = lvSVG.SelectedItems.Count != 0;
+        }
+            
         private void btnConvertClicked(object sender, RoutedEventArgs e)
         {
             foreach (FileItem fileItem in this.lvSVG.SelectedItems)
@@ -104,13 +180,10 @@ namespace SvgConverter
         {
             string wmfFilePath = Path.ChangeExtension(filePath, ".wmf");
 
-            string exePath = typeof(MainWindow).Assembly.Location;
-            string binFolderPath = Path.Combine(Path.GetDirectoryName(exePath), "Inkscape");
-
-            string inkscapePath = Path.Combine(binFolderPath, "inkscape.exe");
+            string inkscapePath = Path.Combine(InkscapePath, "inkscape.exe");
             string arg = string.Format("\"{0}\" --export-wmf=\"{1}\"", filePath, wmfFilePath);
             ProcessStartInfo psi = new ProcessStartInfo(inkscapePath, arg);
-            psi.WorkingDirectory = binFolderPath;
+            psi.WorkingDirectory = InkscapePath;
 
             try
             {
@@ -122,9 +195,8 @@ namespace SvgConverter
             }
             catch (Exception e)
             {
-                throw new ApplicationException(exePath, e);
+                throw new ApplicationException(inkscapePath, e);
             }
         }
-
     }
 }
